f06046a36dfc739d29aac290f7c92812bc76abb8
olvera93 <olvera9302@outlook.com>
2025-10-07 14:29:54 -0600

    [ADD] HomePage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/common/Navbar";
 import Footer from "./components/common/Footer";
 import RegisterPage from "./components/auth/RegisterPage";
 import LoginPage from "./components/auth/LoginPage";
+import HomePage from "./components/home_menu/HomePage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
         </Routes>
       </div>
       <Footer />
